2e59c94a2f2912e965d4364b525f43af2b19222f
ChrisHYKim <ckim96kim@gmail.com>
2024-09-27 09:32:23 +0900

    file size code 수정

--- script/filesize.sh
+++ script/filesize.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ $# -ne 1   ]; then 
-    echo "Usage $0 <filename> <filesize>"
+    echo "Usage: $0 <filename>"
     exit 1
 fi
 
-FILENAME= $1
-FILESIZE=`wc -c < $1`  
+FILENAME="$1"
+FILESIZE=$(wc -c < "$FILENAME") 
 
 # 5120 byte 큰 경우
-if [    "$FILESIZE" -ge 5120 ]; then
+if [ "$FILESIZE" -ge 5120 ]; then
     echo "[ INFO  ] $FILENAME($FILESIZE): Big Size"
 # 5120 byte 작은 경우,
 else
